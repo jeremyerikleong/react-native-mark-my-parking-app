77435cecaf0c24f58178247ccbd76299b444fff7
Jeremy <jeremythegreat95@gmail.com>
2025-10-12 18:14:36 +0800

    tuned status bar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,43 @@
 import BtnBack from '@/components/BtnBack';
 import { CameraPermissionProvider } from '@/context/CameraPermissionContext';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 import { COLORS, SIZES } from '@/constants/theme';
 
 export default function RootLayout() {
     return (
-        <CameraPermissionProvider>
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                    headerStyle: { backgroundColor: COLORS.primary },
-                    headerTintColor: COLORS.secondary,
-                    headerTitleAlign: 'center',
-                }}
-            >
-                <Stack.Screen name="onboarding" />
+        <>
+            <StatusBar style='light' />
+            <CameraPermissionProvider>
+                <Stack
+                    screenOptions={{
+                        headerShown: false,
+                        headerStyle: { backgroundColor: COLORS.primary },
+                        headerTintColor: COLORS.secondary,
+                        headerTitleAlign: 'center',
+                    }}
+                >
+                    <Stack.Screen name="onboarding" />
 
-                <Stack.Screen name="(tabs)" />
+                    <Stack.Screen name="(tabs)" />
 
-                <Stack.Screen
-                    name="scanner"
-                    options={() => ({
-                        headerShown: true,
-                        title: 'Scan QR Code',
-                        headerLeft: () => (
-                            <View style={styles.btnBackContainer}>
-                                <BtnBack />
-                            </View>
-                        )
-                    })}
-                />
-            </Stack>
-        </CameraPermissionProvider>
+                    <Stack.Screen
+                        name="scanner"
+                        options={() => ({
+                            headerShown: true,
+                            title: 'Scan QR Code',
+                            headerLeft: () => (
+                                <View style={styles.btnBackContainer}>
+                                    <BtnBack />
+                                </View>
+                            )
+                        })}
+                    />
+                </Stack>
+            </CameraPermissionProvider>
+        </>
     );
 }
 
