b60f75868cd6a302cd569a64cbd2d0b63901d718
Jeremy <jeremythegreat95@gmail.com>
2025-10-17 16:33:08 +0800

    fix: expo router can't read my index by default

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,10 +19,9 @@ export default function RootLayout() {
                         headerTitleAlign: 'center',
                     }}
                 >
+                    <Stack.Screen name="index" />
                     <Stack.Screen name="onboarding" />
-
                     <Stack.Screen name="(tabs)" />
-
                     <Stack.Screen
                         name="scanner"
                         options={() => ({
